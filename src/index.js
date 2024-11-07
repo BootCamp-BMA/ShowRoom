211973e430561290fde4ece4f362bdd87b47c378
MohamedMegdoud <megdoudmohamedelhadj@gmail.com>
2024-11-07 22:40:34 +0100

    updating

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import './index.css';
+
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// src/main.js or src/main.ts
 
 root.render(
   <React.StrictMode>
