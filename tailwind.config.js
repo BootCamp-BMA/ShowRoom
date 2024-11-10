b647e6688e3e50e01d357d2c790e51ed292ddbd5
MohamedMegdoud <megdoudmohamedelhadj@gmail.com>
2024-11-10 22:41:58 +0100

    add variable of grid display this help us to a good show of cars with responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
       colors: {
         'primary': '#5f6FFF',  // Adds a custom color 'primary' to the theme
       },
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fill,minmax(350px,1fr))',  // Adds a responsive grid with min 200px
+        'autoD': 'repeat(auto-fill,minmax(150px,1fr))', // Adds a responsive grid with min 150px
+      }
     },
   },
   plugins: [],
