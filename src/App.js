b7f3aa4b4763d5928f20ff5594a53ae3c3a714b1
MohamedMegdoud <megdoudmohamedelhadj@gmail.com>
2024-11-10 13:24:12 +0100

    Add routes for Home, Login, About, MyProfile, Contact, and CarDetails components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import About from './pages/About'
+import MyProfile from './pages/MyProfile'
+import Contact from './pages/Contact'
+import CarDetails from './components/CarDetails'
 
 const App = () => {
   return (
-    <div className='text-rose-300'>App</div>
+    <div className='mx-4 sm:mx-[10%]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/myProfile' element={<MyProfile/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/Cars/:CarId' element={<CarDetails/>}/>
+        </Routes>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import './index.css';
 
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // src/main.js or src/main.ts
 
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </BrowserRouter>
+  
 );
 
