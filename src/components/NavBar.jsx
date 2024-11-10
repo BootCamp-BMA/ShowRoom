efe53fdad2cdd0acff6b6a6736482ec4e8babb75
MohamedMegdoud <megdoudmohamedelhadj@gmail.com>
2024-11-10 22:52:26 +0100

    small changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,11 +18,11 @@ const NavBar = () => {
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
         <NavLink to="/about">
-          <li className='py-1'  >ABOUT</li>
+          <li className='py-1 '  >About</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
         <NavLink to="/contact">
-          <li className='py-1'>CONTACT</li>
+          <li className='py-1'>Contact</li>
           <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
         </NavLink>
       </ul>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from "./Header";
+import Cars from "./Cars";
 
 const Home = () => {
   return (
     <>
-      
+      <Header/>
+      <Cars/>
     </>
   );
 };
