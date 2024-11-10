6b1478201f8d1ff8b8c63c214c7cf27237a861e7
MohamedMegdoud <megdoudmohamedelhadj@gmail.com>
2024-11-10 14:37:42 +0100

    implement the code of navbar

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import About from './pages/About'
 import MyProfile from './pages/MyProfile'
 import Contact from './pages/Contact'
 import CarDetails from './components/CarDetails'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
-    <div className='mx-4 sm:mx-[10%]'>
+    <div className='mx-2 sm:mx-[5%]'>
+      <NavBar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/login' element={<Login/>}/>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import { NavLink, useNavigate } from "react-router-dom";
+import { CiMenuBurger } from "react-icons/ci";
 const NavBar = () => {
+  const navigate = useNavigate()
   return (
-    <div>NavBar</div>
+    <div className="flex justify-between items-center text-sm py-4 mb-4 border-b border-b-gray-400">
+      <NavLink to="/">
+        <p className='cursor-pointer'>ShowRoom <span className='text-slate-900 '>.</span></p>
+      </NavLink>
+      <ul className='hidden md:flex items-start gap-16 font-semibold'>
+        <NavLink to="/" >
+          <li className='py-1 '>Home</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to="/doctors">
+          <li className='py-1' >All Cars</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to="/about">
+          <li className='py-1'  >ABOUT</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+        </NavLink>
+        <NavLink to="/contact">
+          <li className='py-1'>CONTACT</li>
+          <hr className="border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden" />
+        </NavLink>
+      </ul>
+      <NavLink to='/login'>
+        <button className='hidden md:block rounded-full bg-primary px-6 py-3 text-white font-bold '>Login</button>
+      </NavLink>
+    </div>
   )
 }
 
