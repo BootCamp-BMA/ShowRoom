9b77c2ee135a5e03cdae8ed87d8b94e5583079b4
MohamedMegdoud <megdoudmohamedelhadj@gmail.com>
2024-11-10 22:52:08 +0100

    get the id of car of make filter of all cars and make the logic of cardetails will update this later

--- src/components/CarDetails.jsx
+++ src/components/CarDetails.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { Appcontext } from "../contexts/AppContext";
 
 const CarDetails = () => {
-  return (
-    <div>CarDetails</div>
-  )
-}
+  const { cars } = useContext(Appcontext);
+  const { CarId } = useParams();  // Use `CarId` to match route parameter name
 
-export default CarDetails
+  // Convert `CarId` to a number if your car IDs are numbers
+  const carDetails = cars.find((e) => e.id === CarId);
+
+  return (
+    <div>
+      {carDetails ? (
+        <>
+          <h1>{carDetails.name}</h1>
+          <p>Model: {carDetails.model}</p>
+          <img src={carDetails.image} alt={carDetails.name} />
+          {/* Add more car details as needed */}
+        </>
+      ) : (
+        <p>Car not found</p>
+      )}
+    </div>
+  );
+};
+
+export default CarDetails;
