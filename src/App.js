0f9a7ba524f32726246305cca2361c0221fb16f9
Moh <mohamedzitouni753@gmail.com>
2024-11-15 20:09:31 +0100

    "Added Font Awesome, implemented footer component, and hide it on admin panel"

--- src/App.js
+++ src/App.js
@@ -8,30 +8,36 @@ import Contact from "./pages/Contact";
 import CarDetails from "./components/CarDetails";
 import NavBar from "./components/NavBar";
 import Admin from "./Admin/Admin";
+import Footer from "./components/Footer";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const App = () => {
   const location = useLocation();
   const hideNavBar = location.pathname === "/admin";
+  const hideFooter = location.pathname === "/admin"; // Hide Footer on admin page
+
   return (
     <>
-      <div className='mx-3 sm:mx-[5%]'>
+      <div className="mx-3 sm:mx-[5%]">
         {hideNavBar ? (
           ""
         ) : (
-          <div className='mx-1 sm:mx-[3%]'>
+          <div className="mx-1 sm:mx-[3%]">
             <NavBar />
           </div>
         )}
 
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/myProfile' element={<MyProfile />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/Cars/:CarId' element={<CarDetails />} />
-          <Route path='/admin' element={<Admin />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/myProfile" element={<MyProfile />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/Cars/:CarId" element={<CarDetails />} />
+          <Route path="/admin" element={<Admin />} />
         </Routes>
+        {/* Add Footer here */}
+        {hideFooter ? "" : <Footer />}
       </div>
     </>
   );
